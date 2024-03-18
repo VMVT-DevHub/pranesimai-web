8e24e3b1353c3d7d88c085897ff7b6c5b47217a4
LWangllix <vanglikas.lukas@gmail.com>
2024-03-18 14:37:06 +0200

    disconnect 'files' path

--- manifest.js
+++ manifest.js
@@ -2,7 +2,7 @@ export const manifestForPlugIn = {
   registerType: 'prompt',
   includeAssests: ['favicon.ico', 'apple-touc-icon.png', 'masked-icon.svg'],
   workbox: {
-    navigateFallbackDenylist: [/^\/(api)\/.*/],
+    navigateFallbackDenylist: [/^\/(api|files)\/.*/],
   },
   manifest: {
     name: 'PRANEŠIMAI WEB',
